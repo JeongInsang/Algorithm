d20a0b1c26a564a072ebf5ac0bec92c52ece2282
insang <85445626+JeongInsang@users.noreply.github.com>
2023-01-12 17:25:33 +0900

    [Bronze I] Title: 나무 조각, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.py
@@ -0,0 +1,8 @@
+wood = list(map(int, input().split()))
+
+while wood != [1,2,3,4,5]:
+    for i in range(len(wood)):
+        if i != len(wood) -1:
+            if wood[i] > wood[i+1]:
+                wood[i], wood[i+1] = wood[i+1], wood[i]
+                print(*wood)
