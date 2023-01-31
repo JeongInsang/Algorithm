7c40d4b554d326f3bca0f375a2dabcfafbe556c3
insang <85445626+JeongInsang@users.noreply.github.com>
2023-01-31 21:46:46 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,21 +1,21 @@
-from collections import defaultdict
-
-def solution(id_list, report,k):
+def solution(id_list, report, k):
     answer = []
+    report_dict = {i: [] for i in id_list}  # 유저별 신고 유저
+    user_report = {i: 0 for i in id_list}  # 유저별 신고 당한 횟수
+    block_user = [] # 정지된 유저
     
-    report = list(set(report)) # 중복 신고 제거
-    user = defaultdict(set) # user별 신고한 id 저장
-    cnt = defaultdict(int) # user별 신고당한 횟수 저장
-	
-    for r in report:
-        a,b = r.split() # report의 첫번째 값은 신고자id, 두번째 값은 신고당한 id
-        user[a].add(b) # 신고자가 신고한 id 추가
-        cnt[b] += 1 # 신고당한 id의 신고 횟수 추가
+    report = list(set(report))
+    for i in report:
+        user, report_user = i.split()
+        report_dict[user].append(report_user) 
     
-    for i in id_list:
-        result = 0
-        for u in user[i]: # user가 신고한 id가 k번 이상 신고 당했으면, 받을 메일 추가
-            if cnt[u]>=k:
-                result +=1
-        answer.append(result)
+    for report_user in report_dict.values():
+        for j in report_user:
+            user_report[j] += 1
+            if user_report[j] >= k:
+                block_user.append(j)
+    
+    for i in report_dict.values():
+        answer.append(len(set(block_user) & set(i)))
+
     return answer
