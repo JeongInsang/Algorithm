4e2d5dcc593c1af377d55b807652e85a90a81932
insang <85445626+JeongInsang@users.noreply.github.com>
2023-04-23 20:38:14 +0900

    [D2] Title: 간단한 압축 풀기, Time: 102 ms, Memory: 45,848 KB -BaekjoonHub

--- SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.py
+++ SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.py
@@ -2,27 +2,14 @@ t = int(input())
 
 for tc in range(1, t+1):
     n = int(input())
-    arr = []
-    leng = 0
+    chars = []
     for i in range(n):
         c, k = map(str, input().split())
-        arr.append([c]*int(k))
-        leng += len(arr[i])
-
-    answer = [[] for _ in range((leng//10)+1)]
-    j = 0
-    cnt = 0
-    for i in range(n):
-        while arr[i]:
-            if cnt < 10:
-                answer[j].append(arr[i].pop())
-                cnt += 1
-            else:
-                j += 1
-                cnt = 0
-
+        chars += ([c] * int(k))
     print(f'#{tc}')
-    for i in answer:
-        for j in i:
-            print(j, end='')
-        print()
+    for i, char in enumerate(chars):
+        if i == 0 or i % 10 != 9:
+            print(char, end='')
+        else:
+            print(char)
+    print()
