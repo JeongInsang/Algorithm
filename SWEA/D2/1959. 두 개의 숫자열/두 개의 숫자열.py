5b77d9a0938089806c63d78a9ad08ce0d9685e48
insang <85445626+JeongInsang@users.noreply.github.com>
2023-04-23 20:30:57 +0900

    [D2] Title: 두 개의 숫자열, Time: 102 ms, Memory: 43,780 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
@@ -1,25 +1,17 @@
 t = int(input())
 for tc in range(1, t+1):
-    a_len, b_len = map(int, input().split())
+    n, m = map(int, input().split())
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
 
-    if len(a) < len(b):
-        max_arr = b
-        min_arr = a
-    else:
-        max_arr = a
-        min_arr = b
-
-    arr = [[] for _ in range(len(min_arr))]
-    for i in range(len(min_arr)):
-        for j in range(i, i + (len(max_arr) - len(min_arr) + 1)):
-           arr[i].append(min_arr[i] * max_arr[j])
+    if n > m:
+        n, m = m, n
+        a, b = b, a
 
     max_sum = 0
-    for i in range(len(arr[0])):
-        n_sum = 0
-        for j in range(len(arr)):
-            n_sum += arr[j][i]
-        max_sum = max(max_sum, n_sum)
+    for i in range(m - n + 1):
+        num_sum = 0
+        for j in range(n):
+            num_sum += a[j] * b[i+j]
+        max_sum = max(max_sum, num_sum)
     print(f'#{tc} {max_sum}')
