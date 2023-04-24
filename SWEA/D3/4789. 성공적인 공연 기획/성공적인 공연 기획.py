4d1316e05069a050862d1a50a128da7854465642
insang <85445626+JeongInsang@users.noreply.github.com>
2023-04-24 16:22:41 +0900

    [D3] Title: 성공적인 공연 기획, Time: 133 ms, Memory: 58,048 KB -BaekjoonHub

---
+++ SWEA/D3/4789. 성공적인 공연 기획/성공적인 공연 기획.py
@@ -0,0 +1,17 @@
+t = int(input())
+for tc in range(1, t+1):
+    arr = [int(i) for i in input()]
+    clap = 0
+    answer = 0
+
+    for i in range(len(arr)):
+        if i == 0:
+            clap += arr[i]
+        else:
+            while clap < i:
+                answer += 1
+                arr[i-1] += 1
+                clap += arr[i-1]
+            clap += arr[i]
+
+    print(f'#{tc} {answer}')
