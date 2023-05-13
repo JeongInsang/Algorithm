8e92832dee1fef5d232ea28ea8a788d6ff6102b2
insang <85445626+JeongInsang@users.noreply.github.com>
2023-05-13 19:46:34 +0900

    [D3] Title: 100만 이하의 모든 소수, Time: 2,229 ms, Memory: 65,384 KB -BaekjoonHub

---
+++ SWEA/D3/3131. 100만 이하의 모든 소수/100만 이하의 모든 소수.py
@@ -0,0 +1,10 @@
+def prime(n):
+
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return n
+
+for i in range(2, 1000000):
+    if prime(i):
+        print(prime(i), end = ' ')
