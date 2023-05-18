fc26cbcef0383d11153fccb64b8fe16de2d02e6b
insang <85445626+JeongInsang@users.noreply.github.com>
2023-05-18 18:30:59 +0900

    [D2] Title: 달팽이 숫자, Time: 114 ms, Memory: 43,520 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,21 +1,21 @@
 t = int(input())
-
 for tc in range(1, t+1):
     n = int(input())
     arr = [[0 for _ in range(n)] for _ in range(n)]
-    arr[0][0] = 1
     directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    move = 0
     x, y = 0, 0
-    num = 2
-    while num <= n*n:
+    move = 0
+    i = 1
+    arr[x][y] = i
+
+    while i < n**2:
         dx, dy = x + directions[move][0], y + directions[move][1]
         if 0 <= dx < n and 0 <= dy < n and arr[dx][dy] == 0:
-            arr[dx][dy] = num
+            i += 1
+            arr[dx][dy] = i
             x, y = dx, dy
-            num += 1
         else:
             move = (move + 1) % 4
     print(f'#{tc}')
-    for i in arr:
-        print(*i)
+    for j in arr:
+        print(*j)
