3cb3f865dc9566fa6b9eb395594729ff33b61544
insang <85445626+JeongInsang@users.noreply.github.com>
2023-05-19 15:35:55 +0900

    [D3] Title: 새샘이의 7-3-5 게임, Time: 1,328 ms, Memory: 61,280 KB -BaekjoonHub

--- SWEA/D3/5948. 새샘이의 7－3－5 게임/새샘이의 7－3－5 게임.py
+++ SWEA/D3/5948. 새샘이의 7－3－5 게임/새샘이의 7－3－5 게임.py
@@ -1,13 +1,8 @@
 from itertools import combinations
-
 t = int(input())
 
 for tc in range(1, t+1):
-    nums = list(map(int, input().split()))
     answer = []
-
-    for i in combinations(nums, 3):
+    for i in combinations(list(map(int, input().split())), 3):
         answer.append(sum(i))
-    answer = list(set(answer))
-    answer.sort()
-    print(f'#{tc} {answer[-5]}')
+    print(f'#{tc} {sorted(set(answer), reverse=True)[4]}')
