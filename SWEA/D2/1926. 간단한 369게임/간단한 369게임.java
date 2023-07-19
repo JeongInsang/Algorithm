7b318880b908fee331704f2632797a86bc92f5fc
insang <85445626+JeongInsang@users.noreply.github.com>
2023-07-19 13:15:32 +0900

    [D2] Title: 간단한 369게임, Time: 141 ms, Memory: 21,380 KB -BaekjoonHub

--- SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
+++ SWEA/D2/1926. 간단한 369게임/간단한 369게임.java
@@ -1,23 +1,28 @@
 import java.util.Scanner;
+
 public class Solution {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int n = 0;
+		int n;
 		n = sc.nextInt();
 
 		for(int i = 1; i < n+1; i++) {
 			String num = String.valueOf(i);
-			int cnt = num.length() - num.replace("3", "").length() + num.length() - num.replace("6", "").length() + num.length() - num.replace("9", "").length();
-			if (cnt == 0) {
-				System.out.print(i+" ");
-			}
-			else {
-				for(int j = 0; j < cnt; j++) {
-					System.out.print("-");
-				}
-				System.out.print(" ");
-			}
-		} 
-			
+			int cnt = 0;
+            
+            for(int j = 3; j < 10; j+=3){
+            	cnt += num.length() - num.replace(String.valueOf(j), "").length();
+            }
+            if (cnt == 0) {
+                System.out.print(i+" ");
+            }
+            else {
+                for(int k = 0; k < cnt; k++) {
+                    System.out.print("-");
+                }
+                System.out.print(" ");
+			} 
+        }
 	}
 }
+
