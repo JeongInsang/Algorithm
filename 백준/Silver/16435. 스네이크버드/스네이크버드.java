275385eff9bb7b8093a325155dd8ba68aadd20ca
insang <85445626+JeongInsang@users.noreply.github.com>
2023-08-16 15:24:35 +0900

    [Silver V] Title: 스네이크버드, Time: 272 ms, Memory: 20688 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int l = sc.nextInt();
+		int[] h = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			h[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(h);
+		for (int i = 0; i < n; i++) {
+			if(l >= h[i]) l++;
+			else break;
+		}
+		System.out.println(l);
+		
+	}
+
+}
