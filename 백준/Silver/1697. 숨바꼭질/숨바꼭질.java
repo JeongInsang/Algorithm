f249790c99fb77d859253f0dd7d07090751afe80
insang <85445626+JeongInsang@users.noreply.github.com>
2023-08-21 16:05:08 +0900

    [Silver I] Title: 숨바꼭질, Time: 240 ms, Memory: 21320 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Main {
+	static int n, k, depth;
+	static Queue<int[]> q = new ArrayDeque<>();
+	static boolean[] visited = new boolean[100001];
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		k = sc.nextInt();
+		q.offer(new int[] {n, depth});
+		visited[n] = true;
+		
+		bfs();
+	}
+	
+	private static void bfs() {
+		while(!q.isEmpty()) {
+			int[] temp = q.poll();
+			if(temp[0] == k) {
+				System.out.println(temp[1]);
+				break;
+			}
+			for (int i = 0; i < 3; i++) {
+				if(i == 0) {
+					if(temp[0]*2 < visited.length && !visited[temp[0]*2]) {
+						q.offer(new int[] {temp[0]*2, temp[1]+1});
+						visited[temp[0]*2] = true;
+					}
+				}
+				else if (i == 1) {
+					if(temp[0]+1 < visited.length && !visited[temp[0]+1]) {
+						q.offer(new int[] {temp[0]+1, temp[1]+1});
+						visited[temp[0]+1] = true;
+					}
+				}
+				else {
+					if(temp[0] -1 >= 0 && !visited[temp[0]-1]) {
+						q.offer(new int[] {temp[0]-1, temp[1]+1});
+						visited[temp[0]-1] = true;
+					}
+				}
+			}
+		}
+	}
+}
