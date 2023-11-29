42fbfb57b9cbe32003633883b8ede71602e8d1f5
insang <85445626+JeongInsang@users.noreply.github.com>
2023-11-29 23:30:40 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 19,383 ms, Memory: 35,372 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,14 +1,11 @@
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Solution {
-	static int n;
+	static int n, answer;
+	static int[][] map, arr;
 	static int[] dr = {0, 1, 0, -1};
 	static int[] dc = {1, 0, -1, 0};
-	static int[][] map, dis;
-	static Queue<int[]> q;
-	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
@@ -16,37 +13,36 @@ public class Solution {
 		for (int tc = 1; tc <= t; tc++) {
 			n = sc.nextInt();
 			map = new int[n][n];
-			dis = new int[n][n];
-			
+			answer = Integer.MAX_VALUE;
 			for (int i = 0; i < n; i++) {
-				String[] str = sc.next().split(""); 
+				String[] temp = sc.next().split("");
 				for (int j = 0; j < n; j++) {
-					map[i][j] = Integer.parseInt(str[j]);
-					dis[i][j] = Integer.MAX_VALUE;
+					map[i][j] = Integer.parseInt(temp[j]);
 				}
 			}
-			dis[0][0] = 0; 
-			q = new ArrayDeque<int[]>();
-			q.add(new int[] {0, 0});
-			bfs();
-			System.out.printf("#%d" + " " + dis[n-1][n-1], tc);
-			System.out.println();
+			arr = new int[n][n];
+			for (int i = 0; i < n; i++) {
+				Arrays.fill(arr[i], Integer.MAX_VALUE);
+			}
+			arr[0][0] = 0;
+			dfs(0, 0);
+			System.out.println("#" + tc + " " + answer);
 		}
 	}
-	private static void bfs() {
-		while(!q.isEmpty()) {
-			int[] cur = q.poll();
-			int r = cur[0];
-			int c = cur[1];
-			
-			for (int i = 0; i < 4; i++) {
-				int nr = r + dr[i];
-				int nc = c + dc[i];
-				if(nr >= 0 && nr < n && nc >= 0 && nc < n) {
-					if(dis[nr][nc] > dis[r][c] + map[r][c]) {
-						dis[nr][nc] = dis[r][c] + map[r][c];
-						q.add(new int[] {nr, nc});
-					}
+	private static void dfs(int r, int c) {
+		if(r == n-1 && c == n-1) {
+			answer = Integer.min(answer, arr[r][c]);
+		}
+		
+		for (int d = 0; d < 4; d++) {
+			int nr = r + dr[d];
+			int nc = c + dc[d];
+			int cost = 0;
+			if(nr>=0 && nr<n && nc>=0 && nc<n) {
+				cost = map[nr][nc];
+				if(cost + arr[r][c] < arr[nr][nc]) {
+					arr[nr][nc] = cost + arr[r][c];
+					dfs(nr, nc);
 				}
 			}
 		}
